755e6432a4aa1d18c940a15b671894255efc9a9b
ponzu3847 <61131262+ponzu3847@users.noreply.github.com>
2021-02-21 23:06:31 +0900

    bug fix

--- common/base_model.py
+++ common/base_model.py
@@ -2,22 +2,14 @@ import sys,os
 sys.path.append('..')
 from common import config
 from common.np import *
-from common import layer_dictionary
+from common.layer_dictionary import *
+from common.layers import *
 from common.optimizer import *
 import pickle
 from common.util import to_cpu,to_gpu,show_distribution
 
 class BaseModel:
-    def __init__(self,input_shape,layer_list,loss_layer=None,show_distribution=False):
-        '''
-        layer_list=[['conv',param_list],['relu',param_list],['conv',param_list],['relu',param_list],['pool',param_list],
-                    ['conv',param_list],['relu',param_list],['conv',param_list],['relu',param_list],['pool',param_list],
-                    ['conv',param_list],['relu',param_list],['conv',param_list],['relu',param_list],['gap',param_list],
-                    ['affine',param_list]]
-        
-        '''
-        layer_dict=layer_dictionary.layer_dict
-        loss_layer_dict=layer_dictionary.loss_layer_dict
+    def __init__(self,input_shape,layer_list,loss_layer=None,weight_decay=None,weight_decay_lambda=0,show_distribution=False):
         
         #Layerの生成
         self.layers=[]
@@ -35,6 +27,8 @@ class BaseModel:
             self.grads+=layer.grads
             
         self.show_distribution=show_distribution
+        self.weight_decay=weight_decay
+        self.weight_decay_lambda=weight_decay_lambda
 
     def predict(self, x, train_flg=False):
         for layer in self.layers:
@@ -59,23 +53,57 @@ class BaseModel:
 
     def forward(self, x, t):
         y = self.predict(x, train_flg=True)
-        if self.loss_layer is not None:
-            loss=self.loss_layer.forward(y, t)
-            isnan=np.isnan(loss)
-            if isnan:
-                print('lossがnanです')
-            return loss
+        if self.weight_decay=='lasso':
+            weight_decay=0
+            for layer in self.layers:
+                if isinstance(layer,(Affine,Convolution)):
+                    W=layer.params[0]
+                    weight_decay+=self.weight_decay_lambda*np.sum(np.abs(W))
+                elif isinstance(layer,ConvResNet):
+                    pass    #未実装
+                else:
+                    continue
+            loss=self.loss_layer.forward(y,t)+weight_decay
+                    
+        elif self.weight_decay=='ridge':
+            weight_decay=0
+            for layer in self.layers:
+                if isinstance(layer,(Affine,Convolution)):
+                    W=layer.params[0]
+                    weight_decay+=0.5*self.weight_decay_lambda*np.sum(W**2)
+                elif isinstance(layer,ConvResNet):
+                    pass    #未実装
+                else:
+                    continue
+            loss=self.loss_layer.forward(y,t)+weight_decay
+            
         else:
-            return y
+            loss=self.loss_layer.forward(y, t)
         
-    def backward(self,dout):
-        if self.loss_layer is not None:
-            dout = self.loss_layer.backward(dout)
-            isnan=np.isnan(dout)
-            if np.count_nonzero(isnan)!=0:
-                print(self.loss_layer.__class__.__name__+':勾配にnanが含まれます')
+        isnan=np.isnan(loss)
+        if isnan:
+            print('lossがnanです')
+        return loss
+        
+    def backward(self,dout=1):
+        dout = self.loss_layer.backward(dout)
+        isnan=np.isnan(dout)
+        if np.count_nonzero(isnan)!=0:
+            print(self.loss_layer.__class__.__name__+':勾配にnanが含まれます')
         for layer in reversed(self.layers):
             dout = layer.backward(dout)
+            if self.weight_decay=='lasso':
+                if isinstance(layer,(Affine,Convolution)):
+                    layer.grads[0]+=self.weight_decay_lambda
+                elif isinstance(layer,ConvResNet):
+                    pass #未実装
+                
+            elif self.weight_decay=='ridge':
+                if isinstance(layer,(Affine,Convolution)):
+                    layer.grads[0]+=self.weight_decay_lambda*layer.params[0]
+                elif isinstance(layer,ConvResNet):
+                    pass #未実装
+
             isnan=np.isnan(dout)
             if np.count_nonzero(isnan)!=0:
                 print(layer.__class__.__name__+':勾配にnanが含まれます')
@@ -95,7 +123,7 @@ class BaseModel:
 
         return acc / x.shape[0]
 
-    def save_params(self,file_name='params.pkl'):
+    def save_params(self,file_name='DeepConvnet.pkl'):
         params = [p.astype(np.float16) for p in self.params]
 
         if GPU:
@@ -104,7 +132,7 @@ class BaseModel:
         with open(file_name,'wb') as f:
             pickle.dump(params,f)
 
-    def load_params(self,file_name='params.pkl'):
+    def load_params(self,file_name='DeepConvnet.pkl'):
         with open(file_name,'rb') as f:
             params=pickle.load(f)
 
